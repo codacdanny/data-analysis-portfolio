c10804fc8bb54a7a8212ce5759d9ca8f48e7650a
Chinemerem Daniel <danpresh302@gmail.com>
2024-01-09 14:48:09 +0100

    started hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
-import './App.css'
+import { Box } from "@chakra-ui/react";
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <Box bgColor="#fafcfb">
+      <Nav />
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,43 @@
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Link,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+
+const Nav = () => {
+  return (
+    <Grid
+      templateColumns="repeat(5, 1fr)"
+      alignItems="center"
+      padding="1rem 2rem"
+      borderBottom=".5px solid #e0e0e0">
+      <GridItem colSpan={2}>
+        <Heading>Analeyes</Heading>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <List display="flex" gap="3rem">
+          <Link>
+            <ListItem>About</ListItem>
+          </Link>
+          <Link>
+            <ListItem>Services</ListItem>
+          </Link>
+
+          <Link>
+            <ListItem>Portfolio</ListItem>
+          </Link>
+          <Link>
+            <ListItem>Contact</ListItem>
+          </Link>
+        </List>
+      </GridItem>
+      <Box />
+    </Grid>
+  );
+};
+
+export default Nav;
